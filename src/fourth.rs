184d7394f58f73f5585d7e61d3ab101dd51fa692
Tyler Holt <75207702+tlrhlt@users.noreply.github.com>
2026-01-15 22:12:56 -0500

    wip implement a bad safe deque

---
+++ src/fourth.rs
@@ -0,0 +1,32 @@
+use std::{cell::RefCell, rc::Rc};
+
+
+pub struct List<T> {
+    head: Link<T>,
+    tail: Link<T>,
+}
+
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+    prev: Link<T>,
+}
+
+impl<T> Node<T> {
+    fn new(elem: T) -> Rc<RefCell<Self>> {
+        let new_node = Node {
+            elem: elem,
+            next: None,
+            prev: None,
+        };
+        Rc::new(RefCell::new(new_node))
+    }
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { head: None, tail: None }
+    }
+}
